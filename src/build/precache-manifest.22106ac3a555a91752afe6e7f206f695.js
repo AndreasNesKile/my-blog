1de1444df2e1842aca1be2e190a327a991c8b922
Andreas Nes Kile <andrenk@stud.ntnu.no>
2020-02-23 15:23:06 +0100

    Initial commit

---
+++ src/build/precache-manifest.22106ac3a555a91752afe6e7f206f695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b93b6cd77af27b194edc4494ae683f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ee77e7409eb47facd3",
+    "url": "/static/css/main.af53e4af.chunk.css"
+  },
+  {
+    "revision": "10d646aecb430d19dfb3",
+    "url": "/static/js/2.a5ec2b23.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a5ec2b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ee77e7409eb47facd3",
+    "url": "/static/js/main.acce9c09.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
